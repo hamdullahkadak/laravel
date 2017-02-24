62aa9ea89a2bedf1dfb4c96d302ae744deb78dff
hamdullahkadak <hamdullahkadak@gmail.com>
2017-02-24 19:59:01 +0300

    Proje Devam Ediyor

---
+++ resources/views/backend/pages/dashboard.blade.php
@@ -0,0 +1,10 @@
+@extends('backend.app')
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <div class="white-box">
+                <h3 class="box-title">Burası Dashboard</h3>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('backend.app');
+});
+
+Route::get('/admin/dashboard', function () {
+    return view('backend.pages.dashboard');
+});
+
